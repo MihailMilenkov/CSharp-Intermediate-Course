a51651bcdef58dea53e60eee3c9e1c7ddd503042
Mihail Milenkov <mihailmilenkov681@gmail.com>
2023-10-10 11:55:59 +0300

    Added some functionality.

--- CSharpIntermediateCourse/Program.cs
+++ CSharpIntermediateCourse/Program.cs
@@ -1,19 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-
 StopWatch stopWatch = new StopWatch();
 
 while (true)
 {
-    Console.WriteLine("To start the watch press \"1\".");
-    Console.WriteLine("To start the watch press \"2\".");
-    Console.WriteLine("To start the watch press \"3\".");
+    Console.WriteLine("Press \"1\" to start, \"2\" to stop and \"3\" to exit.");
     Console.WriteLine("-------------------------------");
     int inputNum = int.Parse(Console.ReadLine());
 
     if (inputNum == 1)
-        break;
+        break;// stopWatch.Stop();
     else if (inputNum == 2)
         break;
     else
@@ -21,5 +16,21 @@ while (true)
 }
 public class StopWatch
 {
-    
+    private TimeSpan timeExpanded = TimeSpan.Zero;
+
+    public StopWatch()
+    {
+    }
+
+    public void Start()
+    {
+        var started = DateTime.Now;
+        Console.WriteLine($"Watch started at {started}");
+    }
+    public void Stop()
+    {
+        //timeExpanded += DateTime.Now - started;
+        Console.WriteLine($"Watch stopped at {DateTime.Now}");
+        Console.WriteLine($"Time expanded: {timeExpanded}");
+    }
 }
