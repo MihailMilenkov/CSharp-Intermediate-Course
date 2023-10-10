b9ddc31bb76edb413655e92b8ad8140e84dd4668
Nbh <Nbh@DESKTOP-4OF1H96>
2023-10-10 11:34:49 +0300

    Add project files.

---
+++ CSharpIntermediateCourse/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+StopWatch stopWatch = new StopWatch();
+
+while (true)
+{
+    Console.WriteLine("To start the watch press \"1\".");
+    Console.WriteLine("To start the watch press \"2\".");
+    Console.WriteLine("To start the watch press \"3\".");
+    Console.WriteLine("-------------------------------");
+    int inputNum = int.Parse(Console.ReadLine());
+
+    if (inputNum == 1)
+        break;
+    else if (inputNum == 2)
+        break;
+    else
+        break;
+}
+public class StopWatch
+{
+    
+}
