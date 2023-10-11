c75eef0623cf208b6b90d152f6e2670b3250c407
Mihail Milenkov <mihailmilenkov681@gmail.com>
2023-10-11 12:25:33 +0300

    Added new exercise StackOverflowPost.

--- CSharpIntermediateCourse/Program.cs
+++ CSharpIntermediateCourse/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //StopWatch stopWatch = new StopWatch();
 //stopWatch.TurnOn();
+StackOverflowPost post = new StackOverflowPost();
+//post.TurnOn();
+
+public class StackOverflowPost
+{
+    private int voteValue = 0;
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    public int VoteValue { get => voteValue; }
+}
