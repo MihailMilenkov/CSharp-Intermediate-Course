ce445cdc304deef4f3b61a395bdf471974a475c1
Mihail Milenkov <mihailmilenkov681@gmail.com>
2023-10-11 12:53:53 +0300

    The post is in separate file now.

--- CSharpIntermediateCourse/Program.cs
+++ CSharpIntermediateCourse/Program.cs
@@ -1,41 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //StopWatch stopWatch = new StopWatch();
 //stopWatch.TurnOn();
-StackOverflowPost post = new StackOverflowPost();
-post.TurnOn();
-
-public class StackOverflowPost
-{
-    private int voteValue = 0;
-
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public DateTime CreatedOn { get; set; }
-
-    public int VoteValue { get => voteValue; }
-
-    internal void TurnOn()
-    {
-        while (true)
-        {
-            Console.WriteLine("Press \"1\" to UpVote, \"2\" to DownVote and \"3\" to exit.");
-            Console.WriteLine("-------------------------------");
-            int inputNum = int.Parse(Console.ReadLine());
-
-            if (inputNum == 1)
-                Vote(1);
-            else if (inputNum == 2)
-                Vote(-1);
-            else
-            {
-                Console.WriteLine($"Current votes: {voteValue}");
-                break;
-            }
-        }
-    }
-
-    internal void Vote(int vote)
-    {
-        this.voteValue += vote;
-    }
-}
+//StackOverflowPost post = new StackOverflowPost();
+//post.TurnOn();
---
+++ CSharpIntermediateCourse/StackOverflowPost.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class StackOverflowPost
+{
+    private int voteValue = 0;
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedOn { get; set; }
+
+    public int VoteValue { get => voteValue; }
+
+    internal void TurnOn()
+    {
+        while (true)
+        {
+            Console.WriteLine("Press \"1\" to UpVote, \"2\" to DownVote and \"3\" to exit.");
+            Console.WriteLine("-------------------------------");
+            int inputNum = int.Parse(Console.ReadLine());
+
+            if (inputNum == 1)
+                Vote(1);
+            else if (inputNum == 2)
+                Vote(-1);
+            else
+            {
+                Console.WriteLine($"Current votes: {voteValue}");
+                break;
+            }
+        }
+    }
+
+    internal void Vote(int vote)
+    {
+        this.voteValue += vote;
+    }
+}
